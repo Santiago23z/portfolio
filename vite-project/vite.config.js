da341aea19150e3ef74493ce2282c5b947c57f0b
Santiago Zapata <santiagozapata@Santiagos-MacBook-Pro.local>
2023-09-19 16:47:15 -0500

    E

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    // ... Otras configuraciones de compilación ...
-  },
-  // Agrega ".mov" a assetsInclude
-  assetsInclude: ["**/*.mov"],
 })
