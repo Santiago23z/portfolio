2582f003f09bcfa94dc2fa88451338aec8053af9
Santiago Zapata <santiagozapata@Santiagos-MacBook-Pro.local>
2023-10-22 15:57:33 -0500

    updates

--- src/components/infoMe/index.jsx
+++ src/components/infoMe/index.jsx
@@ -1,33 +1,34 @@
 import React from 'react'
 import img from './Ellipse 7.png';
 import selector from './selector.png'
-import kary from '../projectss/assets/kary.png'
-import bot from '../projectss/assets/Screen Recording 2023-09-14 at 10.02.32 PM.mov'
-import todos from '../projectss/assets/todos.png'
-import { Link } from 'react-router-dom';
+
 const YoInfo = () => {
-    return (
-        <div className='w-full ' id='aboutMe'>
-            <figure>
-                <img src={selector} alt="" />
-            </figure>
+  return (
+    <div className=' w-[100%]  grid place-items-center ' id='aboutMe'>
+        <figure>
+            <img src={selector} alt="" />
+        </figure>
+        {/* <p className='bg-white w-full'>
+            <span>2021</span>
+            <span>2022</span>
+            <span>2022</span>
+        </p> */}
+        <div className='w-[100%]  h-fit max-[500px]:min-h-[450px] grid place-items-center bg-[#58469E]'>
+        <h1 className='text-white my-5 text-4xl text-center max-[800px]:text-3xl'>Sobre mí</h1>
+      <aside className='w-[60%] relative max-[800px]:w-full  overflow-x-auto overscroll-x-contain snap-proximity snap-x flex min-[800px]:my-20'>
+                <aside className='w-[48%] max-[800px]:min-w-full px-5'>
+                    <h2 className='text-center text-3xl text-white m-4'>2022</h2>
+                    <p className='text-white text-[15px]'>Completé un bootcamp de fundaciones del software que duró 11 meses, con una dedicación de 60 horas semanales. Aprendí los fundamentos de programación y trabajé en proyectos utilizando C, Python y JavaScript, siendo este último mi favorito. Certifiqué mis habilidades al 100% al finalizar el bootcamp, representando un año de aprendizaje y superación personal significativo.</p>
+                </aside>
 
-            <div className='w-[100%]  h-fit grid place-items-center bg-[#5B44A4] p-6'>
-                <h1 className='text-white text-center text-2xl'>Sobre mí</h1>
-                <section className=' flex overflow-x-auto overscroll-x-contain snap-proximity snap-x w-full gap-9 '>
-                    <aside className='py-5 rounded-md min-w-full max-w-[500px] snap-center'>
-                        <h3 className='text-center w-full text-white text-4xl my-4'>2022</h3>
-                        <p className=' text-stone-100 text-[15px] text-justify'>Completé un bootcamp de fundaciones del software que duró 11 meses, con una dedicación de 60 horas semanales. Aprendí los fundamentos de programación y trabajé en proyectos utilizando C, Python y JavaScript, siendo este último mi favorito. Certifiqué mis habilidades al 100% al finalizar el bootcamp, representando un año de aprendizaje y superación personal significativo.</p>
-                    </aside>
-
-                    <aside className='py-5 rounded-md min-w-full max-w-[500px] snap-center'>
-                        <h3 className='text-center w-full text-white text-4xl my-4'>2023</h3>
-                        <p className=' text-stone-100 text-[15px] text-justify'>Comencé mi carrera como freelancer este año. En febrero, contribuí al desarrollo de un plugin de Figma, centrándome en el front-end. Luego, desde mayo hasta ahora, he trabajado como freelancer en icunox, donde automatizo tareas y desarrollo bots de Telegram con Node.js. También he continuado creciendo como desarrollador front-end en proyectos independientes.</p>
-                    </aside>
-                </section>
-            </div>
+                <aside className='w-[48%] max-[800px]:min-w-full px-5'>
+                    <h2 className='text-center text-3xl text-white m-4'>2023</h2>
+                    <p className='text-white text-[15px]'>Comencé mi carrera como freelancer este año. En febrero, contribuí al desarrollo de un plugin de Figma, centrándome en el front-end. Luego, desde mayo hasta ahora, he trabajado como freelancer en icunox, donde automatizo tareas y desarrollo bots de Telegram con Node.js. También he continuado creciendo como desarrollador front-end en proyectos independientes.</p>
+                </aside>
+        </aside>
         </div>
-    )
+    </div>
+  )
 }
 
 export default YoInfo
